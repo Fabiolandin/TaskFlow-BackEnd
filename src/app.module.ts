7a2b11f39a575d6cd410ef45065215950cce0c9b
Fabio Landin <flandin1990@gmail.com>
2026-08-17 17:34:18 -0300

    feat: criando create update e entity de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsuariosModule],
   controllers: [],
   providers: [],
 })
