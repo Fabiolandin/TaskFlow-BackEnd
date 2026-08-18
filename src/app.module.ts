e1433cba13b0ba3b346613b2ba22a61aa93b15c8
Fabio Landin <flandin1990@gmail.com>
2026-08-18 14:25:42 -0300

    feat: adicionando novos módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { ProjetosModule } from './projetos/projetos.module';
+import { TarefasModule } from './tarefas/tarefas.module';
+import { LabelsModule } from './labels/labels.module';
 
 @Module({
-  imports: [DatabaseModule, UsuariosModule, ProjetosModule],
+  imports: [DatabaseModule, UsuariosModule, ProjetosModule, TarefasModule, LabelsModule],
   controllers: [],
   providers: [],
 })
